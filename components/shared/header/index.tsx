1f9184554a18ed1e500745c8b1b34cad89f391b7
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-12-22 19:57:17 -0800

    Theme Mode Toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import ModeToggle from './mode-toggle'
 
 const Header = () => {
   return (
@@ -23,14 +24,15 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+            <ModeToggle />
             <Button asChild variant='ghost'>
                 <Link href="/cart">
-                    <ShoppingCart size={24} /> Cart
+                    <ShoppingCart/> Cart
                 </Link>
             </Button>
-            <Button asChild variant='ghost'>
+            <Button asChild>
                 <Link href="/sign-in">
-                    <UserIcon size={24} /> Sign In
+                    <UserIcon/> Sign In
                 </Link>
             </Button>
         </div>
