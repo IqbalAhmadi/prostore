9ea6deb887fd80a7f2a7a5fd984f18e6460ee84e
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-12-23 20:32:44 -0800

    Load Products From Databas - error fixed

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -23,9 +23,9 @@ const ProductCard = ({ product }: { product: any }) => {
             <h2 className='text-sm font-medium'>{product.name}</h2>
         </Link>
         <div className="flex-between gap-4">
-            <p>{product.rating} Stars</p>
+            <p>{Number(product.rating)} Stars</p>
             { product.stock > 0 ? (
-                <ProductPrice value={product.price} />
+                <ProductPrice value={Number(product.price)} />
             ) : (
                 <p className="text-destructive">Out Of Stock</p>
             )}
