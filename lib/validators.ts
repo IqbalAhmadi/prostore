4acf33ee64f05b03e27d215c6858d34ff0631498
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-12-24 21:29:36 -0800

    Sign In & Sign Out Action

--- lib/validators.ts
+++ lib/validators.ts
@@ -2,11 +2,11 @@ import { z } from 'zod'
 import { formatNumberWithDecimal } from './utils'
 
 const currency = z
-.string()
-.refine(
-  (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
-  'Price must have exactly 2 decimal places'
-)
+  .string()
+  .refine(
+    (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+    'Price must have exactly 2 decimal places'
+  )
 
 // Schema for inserting products
 export const insertProductSchema = z.object({
@@ -20,4 +20,10 @@ export const insertProductSchema = z.object({
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
+})
+
+// Schema for sigining users in
+export const signInFormSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
 })
