de1625aa2a5f2a4e38e618ffb03ef7336e4eb4fb
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-12-25 15:42:55 -0800

    Sign Up Zod Schema & Action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,14 @@
 'use server'
 
-import { signInFormSchema } from '../validators'
+import { signInFormSchema, signUpFormSchema } from '../validators'
 import { signIn, signOut } from '@/auth'
+import { hashSync } from 'bcrypt-ts-edge'
+import { prisma } from '@/db/prisma'
 // import {isRedirectError} from 'next/dist/client/components/redirect'
 
 // Custom implementation of isRedirectError
 function isRedirectError(error: unknown): boolean {
-  return typeof error === 'object' && error !== null && 'url' in error;
+  return typeof error === 'object' && error !== null && 'url' in error
 }
 
 // Sign in the user with the provided credentials
@@ -22,17 +24,55 @@ export async function signInWithCredentials(
 
     await signIn('credentials', user)
 
-    return { success: true, message: 'Signed in successfully' };
+    return { success: true, message: 'Signed in successfully' }
   } catch (error) {
     if (isRedirectError(error)) {
       // console.log(error)
-      throw error;
+      throw error
     }
-    return { success: false, message: 'Invalid email or password' };
+    return { success: false, message: 'Invalid email or password' }
   }
 }
 
 // Sign out the current user
 export async function signOutUser() {
   await signOut()
+}
+
+// Sign up the user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    })
+
+    const plainPassword = user.password
+
+    user.password = hashSync(user.password, 10)
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    })
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    })
+
+    return { success: true, message: 'Signed up successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      // console.log(error)
+      throw error
+    }
+    return { success: false, message: 'User was not registered' }
+
+  }
 }
