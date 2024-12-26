dabf71fd502a7fd5504ceea4ff0637d921fba6f6
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-12-25 20:26:40 -0800

    typo error - deployment issues

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -3,7 +3,7 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { signInDefaultVlaues } from '@/lib/constants';
+import { signInDefaultValues } from '@/lib/constants';
 import Link from 'next/link';
 import { useActionState } from 'react';
 import { useFormStatus } from 'react-dom';
@@ -41,7 +41,7 @@ const CredentialsSignInForm = () => {
             type='email'
             required
             autoComplete='email'
-            defaultValue={signInDefaultVlaues.email}
+            defaultValue={signInDefaultValues.email}
           />
         </div>
         <div>
@@ -52,7 +52,7 @@ const CredentialsSignInForm = () => {
             type='password'
             required
             autoComplete='password'
-            defaultValue={signInDefaultVlaues.password}
+            defaultValue={signInDefaultValues.password}
           />
         </div>
         <div>
