ddff3c4795ce283ecd40b4329b97bf8ff077d23e
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2025-01-06 13:32:39 -0800

    Payment Method Action & Zod Schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  paymentMethodSchema
 } from '../validators'
 import { auth, signIn, signOut } from '@/auth'
 import { hashSync } from 'bcrypt-ts-edge'
@@ -11,6 +12,7 @@ import { prisma } from '@/db/prisma'
 import { formatError } from '../utils'
 import { ShippingAddress } from '@/types'
 import { isRedirectError } from "next/dist/client/components/redirect-error"
+import { z } from 'zod'
 
 // Sign in the user with the provided credentials
 export async function signInWithCredentials(
@@ -110,5 +112,33 @@ export async function updateUserAddress(data: ShippingAddress) {
     }
   } catch (error) {
     return { success: false, message: formatError(error) }
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error('User not found');
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return {
+      success: true,
+      message: 'User updated successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
