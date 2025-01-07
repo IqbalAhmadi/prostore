e58a18e49e79a347d65a5219725d913c67142372
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2025-01-07 15:50:06 -0800

    Payment Method Form & Update - issue persist

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,14 +4,14 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
-  paymentMethodSchema
+  paymentMethodSchema,
 } from '../validators'
 import { auth, signIn, signOut } from '@/auth'
 import { hashSync } from 'bcrypt-ts-edge'
 import { prisma } from '@/db/prisma'
 import { formatError } from '../utils'
 import { ShippingAddress } from '@/types'
-import { isRedirectError } from "next/dist/client/components/redirect-error"
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { z } from 'zod'
 
 // Sign in the user with the provided credentials
@@ -72,7 +72,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
     return { success: true, message: 'Signed up successfully' }
   } catch (error) {
     if (isRedirectError(error)) {
-      // console.log(error)
       throw error
     }
     return { success: false, message: formatError(error) }
@@ -120,25 +119,25 @@ export async function updateUserPaymentMethod(
   data: z.infer<typeof paymentMethodSchema>
 ) {
   try {
-    const session = await auth();
+    const session = await auth()
     const currentUser = await prisma.user.findFirst({
       where: { id: session?.user?.id },
-    });
+    })
 
-    if (!currentUser) throw new Error('User not found');
+    if (!currentUser) throw new Error('User not found')
 
-    const paymentMethod = paymentMethodSchema.parse(data);
+    const paymentMethod = paymentMethodSchema.parse(data)
 
     await prisma.user.update({
       where: { id: currentUser.id },
       data: { paymentMethod: paymentMethod.type },
-    });
+    })
 
     return {
       success: true,
       message: 'User updated successfully',
-    };
+    }
   } catch (error) {
-    return { success: false, message: formatError(error) };
+    return { success: false, message: formatError(error) }
   }
 }
