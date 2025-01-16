dd4d0c3ab5511b523ed882cd2a3218a8c6777d66
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2025-01-16 00:12:37 -0800

    Uploading Configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    // Will allow prodcution build if there are type errors
-    ignoreBuildErrors: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+      },
+    ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
